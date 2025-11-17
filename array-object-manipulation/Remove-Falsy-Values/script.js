96126fdb955f89a0f8a1f9f652071920a3efd4f5
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-11-17 05:56:53 +0500

    Add JavaScript code to identify falsy values in an array

---
+++ array-object-manipulation/Remove-Falsy-Values/script.js
@@ -0,0 +1,12 @@
+// null, undefined, 0, "", false, NaN
+
+var arr = [1, 2, "Hello", true, "Bye", false, null, 3, undefined, true, 7, 8, "", "What?", " ", NaN, "..", true, 0];
+var falsyValues = [];
+
+for (let i = 0; i < arr.length; i++) {
+    if (!arr[i]) {
+        falsyValues.push(arr[i]);
+    }
+}
+
+console.log(arr);

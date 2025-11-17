2a6a746aab2684384b57c39dfd09776ca8fc22fe
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-11-17 05:58:16 +0500

    Add more falsy values to the test array

--- array-object-manipulation/Remove-Falsy-Values/script.js
+++ array-object-manipulation/Remove-Falsy-Values/script.js
@@ -1,6 +1,6 @@
 // null, undefined, 0, "", false, NaN
 
-var arr = [1, 2, "Hello", true, "Bye", false, null, 3, undefined, true, 7, 8, "", "What?", " ", NaN, "..", true, 0];
+var arr = [1, 2, "Hello", true, "Bye", false, null, 3, undefined, true, 7, 8, "", "What?", " ", NaN, "..", true, 0, 0, false];
 var falsyValues = [];
 
 for (let i = 0; i < arr.length; i++) {
